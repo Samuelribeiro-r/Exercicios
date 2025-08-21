bff9dfe28ecf8ec8d4fe66dcdd5c2cbc3569e66e
Samuel.r <samuel.r.costa9@aluno.senai.br>
2025-08-21 17:27:03 -0300

    Operações Bancarias

---
+++ exercicio 1/script.js
@@ -0,0 +1,26 @@
+var senhaCorreta = "1234"
+var saldo = 1000.00 
+
+
+
+function iniciarOperacoesBancarias() {
+  
+  var senha = prompt("Digite sua senha:")
+  if (senha !== senhaCorreta) {
+    alert("Senha incorreta. Nao vai ser facil assim ahahh!!")
+    return 
+  }
+
+  
+  let continuar = true
+
+  while (continuar) {
+
+    var operacao = prompt("Operação: 1 Saque, 2 Depósito, 3 Sair")
+
+   }
+
+
+}
+
+iniciarOperacoesBancarias() 

9c3a505c1be87808272beb4188f6e4a2a0e8af5b
Samuel.r <samuel.r.costa9@aluno.senai.br>
2025-08-26 17:26:26 -0300

    e

---
+++ exercicio 2/samuel.js
@@ -0,0 +1,25 @@
+var carros = {
+    marca : "Nissan",
+    modelo : "Skyline",
+    ano : "1957",
+  
+}  
+
+console.log(carros.marca)
+carros.ano = 2002
+carros.cor = "Prata"
+
+ console.log(carros)
+
+ var pessoa = {
+    nome : "Samuel",
+    idade : "17",
+    nome: function(){
+        
+    }
+  
+ }
+ console.log(pessoa.nome, "meu nome e", pessoa.nome, "eu tenho", pessoa.idade)
+
+
+

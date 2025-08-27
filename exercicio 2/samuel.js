f6d3bf7358bc11f1bb343263efd9c01a721da6ec
Samuel.r <samuel.r.costa9@aluno.senai.br>
2025-08-27 13:47:33 -0300

    At 2

--- exercicio 2/samuel.js
+++ exercicio 2/samuel.js
@@ -1,25 +1,29 @@
-var carros = {
-    marca : "Nissan",
-    modelo : "Skyline",
-    ano : "1957",
-  
-}  
+const carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2020
+};
 
-console.log(carros.marca)
-carros.ano = 2002
-carros.cor = "Prata"
+console.log("Marca:", carro.marca);
+console.log("Modelo:", carro.modelo);
+console.log("Ano:", carro.ano);
 
- console.log(carros)
 
- var pessoa = {
-    nome : "Samuel",
-    idade : "17",
-    nome: function(){
-        
+console.log("Marca do carro:", carro.marca); 
+
+carro.ano = 2024; 
+carro.cor = "Prata"; 
+
+console.log("Ano atualizado:", carro.ano);
+console.log("Cor do carro:", carro.cor);
+
+
+const pessoa = {
+    nome: "Samuel",
+    idade: 30,
+    apresentar: function() {
+        console.log("Olá meu nome é", pessoa.nome, "Tenho", pessoa.idade, "Anos de idade Algun poblema?")
     }
-  
- }
- console.log(pessoa.nome, "meu nome e", pessoa.nome, "eu tenho", pessoa.idade)
+};
 
-
-
+console.log(pessoa.apresentar());
